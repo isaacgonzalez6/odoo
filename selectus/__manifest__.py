1bea415cd08646d137f07664ec47c6d3b76fac0f
isaacglezr6 <62027250+isaacgonzalez6@users.noreply.github.com>
2021-07-22 12:39:29 -0500

    Add files via upload

---
+++ selectus/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'selectus_xtra',
+    'version': '0.1',
+    'description': 'Modulo para migracion de campos y funcionalidades extras a selectus',
+    'author': 'Meridasoft',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+        'product',
+    ],
+    'data': [
+        'views/product_template_view.xml'
+    ],
+    'auto_install': True,
+    'application': True,
+    'installable':True,
+}
